32794d5e309fc35506d835e78fb007b5913fd42b
Thiago Hirata <hirata@devcase.com.br>
2016-08-24 12:08:44 -0300

    Player Behaviour

---
+++ 2dPlatformer/Assets/Script/PlayerBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+    float jumpSpeed = 10;
+    float movementSpeed = 5;
+	
+	void FixedUpdate () {
+        Rigidbody2D rigidBody2D = this.GetComponent<Rigidbody2D>();
+        Vector2 velocidade = rigidBody2D.velocity;
+        if (Input.GetButtonDown("Jump"))
+        {
+            velocidade.y = jumpSpeed;
+        }
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            velocidade.x = movementSpeed * Input.GetAxis("Horizontal");
+        }
+        rigidBody2D.velocity = velocidade;
+    }
+}
